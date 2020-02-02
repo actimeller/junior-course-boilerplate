0f374ac6bb235e365fe076288b43c40f4208967f
Максим Водянов <max.post@bk.ru>
2020-02-02 15:21:30 +0300

    Legacy

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 import basketPicture from '../../assets/images/basket.svg';
 import checkPicture from '../../assets/images/check.svg';
 import Title from '../Title/Title';
@@ -11,11 +13,26 @@ const Basket = ({list, isLoading, isSaved, isError, handleResetBasket, handleSav
         <div className={cx(s.Basket, {[s.BasketLoading]: isLoading})}>
             <div className={s.BasketHeader}>
                 <img src={basketPicture} alt="Корзина"/>
-                <Title>Корзина</Title>
-                <strong>{list.length}</strong>
+                <Link to='/basket'><Title>Корзина</Title></Link>
                 {isSaved && <img className={s.BasketCheck} src={checkPicture} alt="Saved"/>}
                 {isError && 'Произошла ошибка'}
             </div>
+            {list.length > 0 &&
+            <div className={s.BasketBody}>
+                <div>
+                    <label>Товаров:</label>
+                    <strong>{list.length}</strong>
+                </div>
+                <div>
+                    <label>Всего:</label>
+                    <strong>{
+                        list.reduce(function (sum, current) {
+                            return sum + current.price;
+                        }, 0)
+                    }</strong>
+                </div>
+            </div>
+            }
             <div className={s.BasketControls}>
                 {!isSaved && <Button handleClick={handleSaveBasket} isLoading={isLoading}>Сохранить</Button>}
                 <Button handleClick={handleResetBasket}>Очистить корзину</Button>
---
+++ src/containers/BasketListContainer.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import List from '../components/List/List';
+import ListEmpty from '../components/ListEmpty/ListEmpty';
+import {getFilteredProducts} from '../utils/getFilteredProducts';
+import {productsActions} from '../store/products';
+import {withRouter} from 'react-router';
+
+class BasketListContainer extends React.Component {
+    componentDidMount() {
+        this.fetchProducts('https://course-api.csssr.school/products')
+    }
+
+    fetchProducts = (url) => {
+        this.props.loadProductsStart();
+
+        fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    this.props.loadProductsFail(response.message);
+                }
+            })
+            .then(response => {
+                this.props.loadProductsSuccess(response.products);
+
+            })
+            .catch(error => {
+                this.props.loadProductsFail(error.message);
+            })
+    };
+
+    render() {
+        const {products, isLoading} = this.props;
+
+        if (!isLoading && products < 1) {
+            return (
+                <ListEmpty/>
+            )
+        } else {
+            return (
+                <List
+                    isLoading={isLoading}
+                    products={products}
+                />
+            )
+        }
+    }
+}
+
+const mapStateToProps = ({filter, pagination, router, data, basket}) => ({
+    isLoading: data.isLoading,
+    isError: false.isError,
+    products: basket.list
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    loadProductsStart: () => dispatch(productsActions.loadProductsStart()),
+    loadProductsSuccess: (value) => dispatch(productsActions.loadProductsSuccess(value)),
+    loadProductsFail: (value) => dispatch(productsActions.loadProductsFail(value)),
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(BasketListContainer));
+
